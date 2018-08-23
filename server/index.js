31376f897039b7d3075372a7c1464f86569e36a2
jonjharrison <jj2harrison@gmail.com>
2018-08-22 19:00:46 -0700

    Initial route stubs

--- server/index.js
+++ server/index.js
@@ -4,4 +4,24 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/itineraries', (req, res) => {
+  res.end();
+});
+
+app.post('/itineraries', (req, res) => {
+  res.end();
+});
+
+app.get('/stops', (req, res) => {
+  res.end();
+});
+
+app.get('/user', (req, res) => {
+  res.end();
+});
+
+app.post('/user', (req, res) => {
+  res.end();
+});
+
 app.listen(port, () => console.log(`Server listening on port ${port}`));
