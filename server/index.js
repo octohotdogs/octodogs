97488b8d23c14b072620788d80ad072b475ac3ab
jonjharrison <jj2harrison@gmail.com>
2018-08-28 19:17:34 -0700

    Shell of server

--- server/index.js
+++ server/index.js
@@ -5,23 +5,23 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/itineraries', (req, res) => {
-  res.end();
+  res.end('TODO: Returning a list of itineraries');
 });
 
 app.post('/itineraries', (req, res) => {
-  res.end();
+  res.end('TODO: An itinerary has been created');
 });
 
 app.get('/stops', (req, res) => {
-  res.end();
+  res.end('TODO: Returning a list of stops');
 });
 
 app.get('/user', (req, res) => {
-  res.end();
+  res.end('TODO: Returning a user');
 });
 
 app.post('/user', (req, res) => {
-  res.end();
+  res.end('TODO: Creating a user');
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
