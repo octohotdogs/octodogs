6b7855b4a1e1b0ae2880b16b47d2b1a9353c085d
James Lee <james.k.lee4@gmail.com>
2018-08-29 22:53:35 -0700

    Render markers for each stop in itinerary

--- client-react/api-callers.js
+++ client-react/api-callers.js
@@ -7,4 +7,29 @@ var getGoogleMaps = function(itinerary) {
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 4, center: initLocation});
 
+  // Loop through stops in itinerary and place markers on map
+  itinerary.forEach((stop, i) => {
+
+    // Create marker object
+    var marker = new google.maps.Marker({
+      position: stop.location,
+      map: map,
+      // Blue icon for all stops except first
+      icon: i === 0 ? null : i === itinerary.length - 1 ? 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png' : 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+      // Bouncy icon for first stop
+      animation: i === 0 ? google.maps.Animation.BOUNCE : google.maps.Animation.DROP,
+      // Pop up window to show stop info
+      message: new google.maps.InfoWindow({
+        title: 'Stop ' + i,
+        content: '<div>' +
+          '<h3>' + stop.name + '</h3>' +
+          '<div>' + stop.date + '</div>' +
+          '<div>' + stop.notes + '</div>' +
+          '<div>' + stop.comments + '</div>' +
+          '</div>',
+        maxWidth: 320
+      }),
+    });
+
+  })
 }
