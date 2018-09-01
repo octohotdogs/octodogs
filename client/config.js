e925b84e51cf7e37bf6730276048caccc1ce74b8
Luda,Shashua <luda_sf@yahoo.com>
2018-09-01 16:19:32 -0700

    fixing typo, remove config

--- client/config.js
+++
@@ -1,6 +0,0 @@
-export config = {
-  ClientID: 'VDOYC02UVWL4IHWOD2OA5VJIZ33CMNE3CX0OZUDRB535FWCJ',
-  Secret: 'MQTZGTBNXQOMJR2KT5JFGFWICS3QINJFQPROB52PKKSKGIA5',
-  APIkeyAutocomplete: 'AIzaSyDEsCZ8tc6OOMTp6pvAe6anwAvux2gFEAQ', //key=APIkey to google maps javascript api
-  APIkeyMaps: 'AIzaSyAtoKxwXk0nQ76IqxhPSvUBYId4og5Bwts' // key for google maps
-}
