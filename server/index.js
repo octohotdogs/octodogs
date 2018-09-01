05f2ac906a8393ad69af2384929bf7b7bd3e558b
Luda,Shashua <luda_sf@yahoo.com>
2018-09-01 15:28:36 -0700

    map

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/itineraries', (req, res) => {
   res.end('TODO: Returning a list of itineraries');
@@ -24,5 +27,12 @@ app.get('/user', (req, res) => {
 app.post('/user', (req, res) => {
   res.end('TODO: Creating a user');
 });
+
+console.log("dirname: ", __dirname);
+
+app.use(express.static(path.join(__dirname, '/../client')));
+//app.use(express.static(path.join(__dirname, '/../node_modules')));
+app.use('/bootstrap', express.static(__dirname + '/../node_modules/bootstrap/dist/css'));
+app.use('/scripts', express.static(__dirname + '/../node_modules'));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
