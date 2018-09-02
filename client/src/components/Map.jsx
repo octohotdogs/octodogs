e8246230ca0fe5bc318a038c0ba129b454b8790d
jonjharrison <jj2harrison@gmail.com>
2018-09-01 23:22:10 -0700

    Cleanup, referencing helper function for map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
+import { getGoogleMaps } from '../../../helpers/maps-api.js';
+
+// TODO: REPLACE THIS WITH THE ENDPOINT
+import { itineraries } from '../../../seed_data.js';
 
 class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      map: ''
+      map: false
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.map === false) {
+      getGoogleMaps(itineraries[0]);
+      this.setState({
+        map: true
+      })
     }
   }
 
   render() {
+    /*
     const mapStyle = {
       width: 700,
       height: 550,
       border: '1px gray'
     };
+    */
 
     return (
-      // to change hardcoded value to current location
-      <div className="map">
-        <iframe width="600" height="450" style={mapStyle} src= "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2965.0824050173574!2d-93.63905729999999!3d41.998507000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sWebFilings%2C+University+Boulevard%2C+Ames%2C+IA!5e0!3m2!1sen!2sus!4v1390839289319"allowFullScreen>
-        </iframe>
-      </div>
+      <div id="map"></div>
     );
 
   }
