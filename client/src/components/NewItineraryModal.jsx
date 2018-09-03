fb591d48bfe681936fa4c921e35db8eb988ea4bf
jonjharrison <jj2harrison@gmail.com>
2018-09-03 13:20:34 -0700

    Added new itinerary as a modal

--- client/src/components/NewItineraryModal.jsx
+++ client/src/components/NewItineraryModal.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import Modal from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 import $ from 'jquery';
 
-class ItinerarySubmissionForm extends React.Component {
+const modalStyle = {
+  position: 'fixed',
+  zIndex: 1040,
+  top: 0, bottom: 0, left: 0, right: 0,
+  position: 'absolute',
+  width: 500,
+};
+
+const backdropStyle = {
+  position: 'fixed',
+  top: 0, bottom: 0, left: 0, right: 0,
+  zIndex: 'auto',
+  backgroundColor: '#000',
+  opacity: 0.5
+};
+
+class NewItineraryModal extends React.Component {
   constructor(props) {
     super(props);
 
-    var submit = this.submit.bind(this);
+    this.submit = this.submit.bind(this);
   }
 
   submit() {
@@ -52,18 +68,35 @@ class ItinerarySubmissionForm extends React.Component {
   render() {
     return(
       <div>
-        <h1> New Itinerary </h1>
-        <div>Name: <input type="text" className="data" field="name"></input></div>
-        <div>Description: <input type="text" className="data" field="desc"></input></div>
-        <div>
-          Start Date: <input type="date" className="data" field="start"></input>
-          End Date: <input type="date" className="data" field="end"></input>
-        </div>
-        <div>Privacy Level: <input type="text" className="data" field="privacy"></input></div>
-        <button onClick={submit}>Submit</button>
+      <Modal 
+        show={this.props.show} 
+        onHide={this.props.hide}
+        aria-labelledby="ModalHeader"
+        style={modalStyle}
+        backdropStyle={backdropStyle}
+        animation={false}
+      >
+        <Modal.Header>
+          <Modal.Title>Create New Itinerary</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>Name: <input type="text" className="data" field="name"></input></div>
+          <div>Description: <input type="text" className="data" field="desc"></input></div>
+          <div>
+            Start Date: <input type="date" className="data" field="start"></input><br/>
+            End Date: <input type="date" className="data" field="end"></input>
+          </div>
+          <div>Privacy Level: <input type="text" className="data" field="privacy"></input></div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button bsStyle="primary" onClick={this.submit}>Save</Button>
+          <Button onClick={this.props.hide}>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
       </div>
+      
     )
   }
 }
 
-export default ItinerarySubmissionForm;
+export default NewItineraryModal;
