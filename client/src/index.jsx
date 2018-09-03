cc599827441e0ea40eb7cf147b548e2ebb115725
jonjharrison <jj2harrison@gmail.com>
2018-09-03 11:33:08 -0700

    Index cleanup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button, ButtonGroup } from 'react-bootstrap';
 import $ from 'jquery';
 
 import Map from './components/Map.jsx';
@@ -39,46 +40,6 @@ class App extends React.Component {
   //         this.setState({repos: repos});
   //       });
   // }
-
-  receiveData() {
-  //   console.log("in receiveData");
-
-  //   let self = this;
-  //   fetch('http://localhost:1128/repos', {
-  //         type: "GET",
-  //         //url: "http://localhost:1128/repos",
-  //         //data: {username: term},
-  //       //  success: this.receiveData(data),
-  //         dataType: "json"
-  //       })
-  //     .then(function(response) {
-  //       return response.json();
-  //     })
-  //     .then(function(myJson) {
-  //       console.log("now self:___:", self);
-  //       self.setState({repos: myJson});
-  //     });
-  //     console.log("New state: ", this.state.repos);
-  }
-
-  // console.log("here");
- //    return (<div>
- //       <h1>Trip Planner<h1>
- //        <nav className="navbar">
- //          <div className="col-md-6 offset-md-3">
- //            <Iteneraries getItenerariesList={this.getIteneraries.bind(this)}/>
- //          </div>
- //        </nav>
- //        <div className="row">
- //          <div className="col-md-7">
- //            <Map location={this.state.currentLocation}/>
- //          </div>
- //          <div className="col-md-5">
- //            <RecentItenerary currentItenanary={this.state.currentItenerary}/>
- //          </div>
- //        </div>
- //      </div>)
-
 
   componentDidMount() {
     let self = this;
