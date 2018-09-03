748d963fd2b39023bc10bca61dcd84822804a2ac
jonjharrison <jj2harrison@gmail.com>
2018-09-03 13:20:34 -0700

    Added new itinerary as a modal

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, ButtonGroup } from 'react-bootstrap';
+import { Button, ButtonGroup, Modal } from 'react-bootstrap';
 import $ from 'jquery';
 
 import Map from './components/Map.jsx';
 import Header from './components/Header.jsx';
 import Itineraries from './components/Itineraries.jsx';
+import NewItineraryModal from './components/NewItineraryModal.jsx';
 
 // TODO: REPLACE THIS WITH THE ENDPOINT
 import { itineraries } from '../../seed_data.js';
@@ -16,34 +17,33 @@ class App extends React.Component {
 
     this.state = {
       iteneraries: itineraries,
-      currentItinerary: itineraries[0]
+      currentItinerary: itineraries[0],
+      showItineraryModal: false
     }
 
     //this.search = this.search.bind(this);
     this.getItineraries = this.getItineraries.bind(this);
+    this.openNewItinerary = this.openNewItinerary.bind(this);
+    this.closeNewItinerary = this.closeNewItinerary.bind(this);
+  }
+
+  openNewItinerary() {
+    console.log('Calling new itinerary');
+    this.setState({
+      showItineraryModal: true
+    });
+  }
+
+  closeNewItinerary() {
+    this.setState({
+      showItineraryModal: false
+    });
   }
 
   getItineraries() {
   }
 
-  // search (term) {
-  //   console.log(`${term} was searched`);
-
-  //     $.ajax({
-  //         type: "POST",
-  //         url: "http://localhost:1128/repos",
-  //         data: {username: term},
-  //       //  success: this.receiveData(data),
-  //         dataType: "json"
-  //       }).then((repos) => {
-  //        // this.receiveData();
-  //         this.setState({repos: repos});
-  //       });
-  // }
-
   componentDidMount() {
-    let self = this;
-    this.receiveData();
   }
 
   updateCurrentItinerary() {
@@ -59,10 +59,16 @@ class App extends React.Component {
         </nav>
         <div className="row">
           <div className="col-md-5">
+            <ButtonGroup>
+              <Button bsStyle="primary">Add Stop</Button>
+              <Button>Change Itinerary</Button>
+              <Button onClick={this.openNewItinerary}>Create New Itinerary</Button>
+            </ButtonGroup>
             <Itineraries itineraries={this.state.iteneraries}/>
+            <NewItineraryModal show={this.state.showItineraryModal} hide={this.closeNewItinerary}></NewItineraryModal>
           </div>
           <div className="col-md-7">
-            <Map currentItinerary={this.state.currentItinerary}/>
+            {/* <Map currentItinerary={this.state.currentItinerary}/> */}
           </div>
         </div>
       </div>
