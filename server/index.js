8d600f37c3b9224ca53c601d5577b614cd514bf5
Hao-Han Pan <c.avantin94@gmail.com>
2018-09-03 17:25:18 -0500

    Refactor routes into new routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+
+const usersRouter = require('./routers/users.js');
+const itinerariesRouter = require('./routers/itineraries.js');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -9,24 +14,7 @@ app.use(express.static(path.join(__dirname, '/../client')));
 app.use('/bootstrap', express.static(__dirname + '/../node_modules/bootstrap/dist/css'));
 app.use('/scripts', express.static(__dirname + '/../node_modules'));
 
-app.get('/itineraries', (req, res) => {
-  res.end('TODO: Returning a list of itineraries');
-});
-
-app.post('/itineraries', (req, res) => {
-  res.end('TODO: An itinerary has been created');
-});
-
-app.get('/stops', (req, res) => {
-  res.end('TODO: Returning a list of stops');
-});
-
-app.get('/user', (req, res) => {
-  res.end('TODO: Returning a user');
-});
-
-app.post('/user', (req, res) => {
-  res.end('TODO: Creating a user');
-});
+app.use('/api/users', usersRouter);
+app.use('/api/itineraries', itinerariesRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
