6fa094e311d06c017165409af62a5f0a82e6c41b
Hao-Han Pan <c.avantin94@gmail.com>
2018-09-04 23:02:04 -0500

    Add mockup syntax for POST/PUT routes
    This syntax can be uncommented and used once their corresponding
    controller functions have been created.

--- server/routers/itineraries.js
+++ server/routers/itineraries.js
@@ -5,7 +5,15 @@ const controller = require('../../db/controllers/itinerary.js');
 router.route('/')
   .post(function(req, res) {
     // TODO: This should save a new itinerary to the database
-    res.send('POST on /api/itineraries');
+    // Uncomment the below and delete the extra res.send when the controller is ready.
+    /*
+    controller.saveNewItinerary(itinerary, function(err, result) {
+      if (err) {
+        return console.error(err);
+      }
+      res.send('Itinerary saved.'); // change as needed
+    */
+    res.send('POST on /api/itineraries'); // DELETE ME WHEN DONE
   });
 
 // Get an itinerary by its ID
@@ -23,9 +31,16 @@ router.route('/:itinid')
 router.route('/:itinid')
   .put(function(req, res) {
     let itinId = req.params.itinid;
-    // TODO: This should update an itinerary
-    // (should we use POST or PUT for this?)
-    res.send('POST on /api/itineraries/' + itinId);
+    // TODO: This should update an itinerary non-stop information. Use PUT /:itinid/stops to save new stops
+    // Uncomment the below and delete the extra res.send when the controller is ready.
+    /*
+    controller.updateItineraryInfo(itinId, update, function(err, result) {
+      if (err) {
+        return console.error(err);
+      }
+      res.send('Itinerary updated.'); // change as needed
+    */
+    res.send('POST on /api/itineraries/' + itinId); // DELETE ME WHEN DONE
   });
 
 // Get all stops in an itinerary
@@ -44,7 +59,15 @@ router.route('/:itinid/stops')
   .put(function(req, res) {
     let itinId = req.params.itinid;
     // TODO: This should save a new stop to an itinerary
-    res.send('POST on /api/itineraries/' + itinId + '/stops');
+    // Uncomment the below and delete the extra res.send when the controller is ready.
+    /*
+    controller.saveNewStop(itinId, stop, function(err, result) {
+      if (err) {
+        return console.error(err);
+      }
+      res.send('Stop saved.'); // change as needed
+    */
+    res.send('POST on /api/itineraries/' + itinId + '/stops'); // DELETE ME WHEN DONE
   });
 
 // TODO: Some kind of route to update a stop? Maybe PUT on /:itinid/stops/:index
