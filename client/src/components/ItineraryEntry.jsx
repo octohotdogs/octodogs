4488cf983bc0593e8a7c42781e3c1edabc29c9a8
James Lee <james.k.lee4@gmail.com>
2018-09-04 23:26:37 -0700

    Render name and description for itineraryEntry component

--- client/src/components/ItineraryEntry.jsx
+++ client/src/components/ItineraryEntry.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
+import { ListGroupItem } from 'react-bootstrap';
 
 var ItineraryEntry = (props) => {
-  var onClick = (event) => {
-  //  props.updateCurrentItinerary(props.itinerary);
-  };
 
   return (
-    <div className="itinerary-entry media">
-      <div className="media-body">
-        <div className="itinerary-entry-title" onClick={onClick}>{props.itinerary.id}</div>
-        <div className="itinerary-entry-detail">{props.itinerary.description}</div>
-      </div>
-    </div>
+    <ListGroupItem
+      header={props.itinerary.name}
+    >
+      <div>{props.itinerary.description}</div>
+    </ListGroupItem>
   );
 };
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 ItineraryEntry.propTypes = {
-  itinenary: React.PropTypes.object.isRequired
+  itinerary: React.PropTypes.object.isRequired
 };
-
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
-//window.VideoListEntry = VideoListEntry;
 
 export default ItineraryEntry;
