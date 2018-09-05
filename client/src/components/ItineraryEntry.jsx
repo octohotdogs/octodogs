8489704a9010370ac44c4491766ee20e787270fc
James Lee <james.k.lee4@gmail.com>
2018-09-04 23:27:22 -0700

    Render trip start and end dates for itineraryEntry component

--- client/src/components/ItineraryEntry.jsx
+++ client/src/components/ItineraryEntry.jsx
@@ -3,10 +3,14 @@ import { ListGroupItem } from 'react-bootstrap';
 
 var ItineraryEntry = (props) => {
 
+  var tripStart = props.itinerary.dates.start.toString().slice(0,15)
+  var tripEnd = props.itinerary.dates.end.toString().slice(0,15)
+
   return (
     <ListGroupItem
       header={props.itinerary.name}
     >
+      <div>{tripStart + ' - ' + tripEnd}</div>
       <div>{props.itinerary.description}</div>
     </ListGroupItem>
   );
