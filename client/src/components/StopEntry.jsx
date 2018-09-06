f4436241d6eec912968ac64ad04e7fd1426c6f2c
James Lee <james.k.lee4@gmail.com>
2018-09-05 23:17:28 -0700

    Add StopEntry component

---
+++ client/src/components/StopEntry.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ListGroupItem } from 'react-bootstrap';
+
+var StopEntry = (props) => {
+
+  var stopDate = props.stop.date.toString().slice(0,15)
+
+  return (
+    <ListGroupItem
+      header={props.stop.name}
+    >
+      <div>{stopDate}</div>
+      <div>{props.stop.notes}</div>
+      <div>{props.stop.comments}</div>
+    </ListGroupItem>
+  );
+};
+
+// PropTypes tell other developers what `props` a component expects
+// Warnings will be shown in the console when the defined rules are violated
+StopEntry.propTypes = {
+  stop: React.PropTypes.object.isRequired
+};
+
+export default StopEntry;
