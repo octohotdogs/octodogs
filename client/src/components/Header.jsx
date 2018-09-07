7a031dbc5560953c68c4d32fdcbed5d2047a0220
jonjharrison <jj2harrison@gmail.com>
2018-09-06 20:53:38 -0700

    Modified based on new css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,33 +7,9 @@ class Header extends React.Component {
     super(props);
   }
 
-  // const buttonsInstance = (
-  //  // <ButtonToolbar>{BUTTONS.map(renderDropdownButton)}</ButtonToolbar>
-  // );
-
-  // function renderDropdownButton(title, i) {
-  //   return (
-  //     <DropdownButton
-  //       bsStyle={title.toLowerCase()}
-  //       title={title}
-  //       key={i}
-  //       id={`dropdown-basic-${i}`}
-  //     >
-  //       <MenuItem eventKey="1">Action</MenuItem>
-  //       <MenuItem eventKey="2">Another action</MenuItem>
-  //       <MenuItem eventKey="3" active>
-  //         Active Item
-  //       </MenuItem>
-  //       <MenuItem divider />
-  //       <MenuItem eventKey="4">Separated link</MenuItem>
-  //     </DropdownButton>
-  //   );
-
-  // }
-
   render() {
       return (
-      <Navbar fixedTop>
+      <Navbar inverse fixedTop>
         <Navbar.Header>
           <Navbar.Brand>
             <a href="/">
