f866079029e3a423917bd77991719d60fcf951f3
Hao-Han Pan <c.avantin94@gmail.com>
2018-09-08 14:37:09 -0500

    Build route to save new stop

--- server/routers/itineraries.js
+++ server/routers/itineraries.js
@@ -62,18 +62,15 @@ router.route('/:itinid/stops')
   });
 
 router.route('/:itinid/stops')
-  .put(function(req, res) {
+  .put(jsonParser, function(req, res) {
     let itinId = req.params.itinid;
-    // TODO: This should save a new stop to an itinerary
-    // Uncomment the below and delete the extra res.send when the controller is ready.
-    /*
+    let stop = req.body.stop;
     controller.saveNewStop(itinId, stop, function(err, result) {
       if (err) {
         return console.error(err);
       }
-      res.send('Stop saved.'); // change as needed
-    */
-    res.send('PUT on /api/itineraries/' + itinId + '/stops'); // DELETE ME WHEN DONE
+      res.send(result);
+    });
   });
 
 // TODO: Some kind of route to update a stop? Maybe PUT on /:itinid/stops/:index
