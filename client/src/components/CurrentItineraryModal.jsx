65a796cf7d10c26103bfd9f8dffe270592f7120f
James Lee <james.k.lee4@gmail.com>
2018-09-10 23:22:38 -0700

    Modify modals to position correctly

--- client/src/components/CurrentItineraryModal.jsx
+++ client/src/components/CurrentItineraryModal.jsx
@@ -3,20 +3,12 @@ import StopEntry from './StopEntry.jsx';
 import { Modal, ListGroup, Button } from 'react-bootstrap';
 import $ from 'jquery';
 
-const modalStyle = {
-  position: 'fixed',
-  zIndex: 1040,
-  top: 107, bottom: 0, left: 290, right: 0,
-  width: 441,
-};
-
 const backdropStyle = {
   position: 'fixed',
-  top: 107, bottom: 0, left: 290, right: 0,
-  zIndex: 1000,
-  backgroundColor: '#fff',
+  top: 125, bottom: 0, left: 250, right: 0,
+  zIndex: '1000',
   opacity: 1,
-  width: 441
+  width: 460,
 };
 
 var CurrentItineraryModal = (props) => (
@@ -25,9 +17,9 @@ var CurrentItineraryModal = (props) => (
       show={props.show}
       onHide={props.hide}
       aria-labelledby="ModalHeader"
-      style={modalStyle}
       backdropStyle={backdropStyle}
-      animation={true}
+      animation={false}
+      dialogClassName="custom-modal"
     >
       <Modal.Header>
 
